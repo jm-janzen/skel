8c8c4970c7575fc5296f438602267530769f3b74
JM <jmjanzen@gmail.com>
2025-02-04 09:40:52 -0500

    update polybar
    Add Polybar mail module (with some local edits), plus style tweaks.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 
-echo "---" >> /tmp/polybar-top.log
-polybar top -r -l warning 2>&1 | tee -a /tmp/polybar-top.log & disown
+echo "---" >> /tmp/polybar-bottom.log
+polybar bottom -r -l warning 2>&1 | tee -a /tmp/polybar-bottom.log & disown
 
