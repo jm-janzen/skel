a1db46423402ed75cdf80db0fa6f1363a7f1a58f
JM <hello@jmjanzen.com>
2026-01-15 16:02:53 -0500

    wayland: some wip changes
    Still figuring this out.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,8 +2,8 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+#polybar-msg cmd quit
 
 echo "---" >> /tmp/polybar-bottom.log
-polybar bottom -r -l warning 2>&1 | tee -a /tmp/polybar-bottom.log & disown
+waybar bottom -r -l warning 2>&1 | tee -a /tmp/polybar-bottom.log & disown
 
